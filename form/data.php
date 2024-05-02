c678e977078cb757d0cdac64f489c52efb2c9e12
Elddis09 <adis.anjeli01@gmail.com>
2024-05-02 09:18:12 +0700

    update materi 5 (get/post) metode

---
+++ form/data.php
@@ -0,0 +1,5 @@
+<?php
+echo $_POST['nama_lengkap'] . "<br>";
+echo $_POST['j_kelamin'] . "<br>";
+echo $_POST['kota'] . "<br>";
+echo $_POST['alamat'] . "<br>";
--- form/register.php
+++ form/register.php
@@ -3,37 +3,38 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Akun</title>
 </head>
 
 <body>
-    <form action="">
-        <label for="">Nama Lengkap</label>
-        <input type="text" name="nama_lengkap" placeholder="ex:jaemina">
+    <form action="data.php" method="post">
+        <label>Nama Lengkap:</label>
+        <input type="text" name="nama_lengkap" value="">
 
         <br>
 
-        <label for="">Jenis Kelamin</label>
-        <input type="radio" name="j_kelamin"> Laki Laki
-        <input type="radio" name="j_kelamin"> Perempuan
+        <label>Jenis Kelamin:</label>
+        <input type="radio" name="j_kelamin" value="Laki-Laki"> Laki-Laki
+        <input type="radio" name="j_kelamin" value="Perempuan"> Perempuan
 
         <br>
-
-        <label for="">Kabupaten/Kota</label>
+        <label>Kabupaten/Kota:</label>
         <select name="kota">
-            <option value="kota_bandung">Kota Bandung</option>
-            <option value="kota_bandung">Kab.Bandung</option>
-            <option value="kota_bandung">Kabupaten Bandung Barat</option>
-            <option value="kota_bandung">Kota Cimahi</option>
+            <option value="Kota Bandung">Kota Bandung</option>
+            <option value="Bandung">Bandung</option>
+            <option value="Kab. Bandung Barat">Kabupaten Bandung Barat</option>
+            <option value="Kota Cimahi">Kota Cimahi</option>
         </select>
 
         <br>
-        <label for="">Alamat</label>
-        <textarea name="alamat" id="" cols="30" rows="10"></textarea>
+        <label>Alamat:</label><br>
+        <textarea name="alamat" cols="30" rows="10"></textarea>
 
         <br>
-        <input type="button" value="kirim">
+        <input type="submit" value="Kirim">
+
     </form>
 </body>
 
